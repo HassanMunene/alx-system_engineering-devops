f6a6fad3b3ed724dcf8a3ae5c0e00d0ef4ce0240
HassanMunene <awanzihassan@gmail.com>
2024-07-27 13:04:03 +0300

    puppet

--- 0x0C-web_server/7-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -1,41 +1,69 @@
 # This manifest is used to configure nginx web server during installation
 package { 'nginx':
-    ensure => 'installed',
+  ensure => installed,
 }
 
-exec {'install':
-    command  => 'sudo apt-get update ; sudo apt-get -y install nginx',
-    provider => shell,
-}
+# Ensure the Nginx service is running and enabled
 service { 'nginx':
-    ensure  => 'running',
-    enable  => true,
-    require => Package['nginx'],
+  ensure    => running,
+  enable    => true,
+  subscribe => File['/etc/nginx/sites-available/default'], # Restart service if configuration changes
 }
 
-file {'/etc/nginx/sites-available/default':
-    ensure  => 'file',
-    content => '
-       server {
-           listen 80;
-           listen [::]:80 default_server;
-           root /etc/nginx/html;
-           index.html index.htm;
-       
-            location /redirect_me {
-                return 301 https://www.youtube.com/watch?v=QH2-TGUlwu4;
-            }
-        }
-    ',
-    require => Package['nginx'],
-    notify  => Service['nginx']
-}
-file {'/etc/nginx/html/index.html':
-    ensure  => 'file',
-    content => 'Hello World',
+# Create the directory for the Nginx root
+file { '/etc/nginx/html':
+  ensure => directory,
+  owner  => 'root',
+  group  => 'root',
+  mode   => '0755',
 }
 
-exec {'run':
-    command  => 'sudo service nginx restart',
-    provider => shell,
+# Create the index.html file
+file { '/etc/nginx/html/index.html':
+  ensure  => file,
+  owner   => 'root',
+  group   => 'root',
+  mode    => '0644',
+  content => "Hello World!\n",
+}
+
+# Create the 404.html file
+file { '/etc/nginx/html/404.html':
+  ensure  => file,
+  owner   => 'root',
+  group   => 'root',
+  mode    => '0644',
+  content => "Ceci n'est pas une page\n",
+}
+
+# Configure Nginx
+file { '/etc/nginx/sites-available/default':
+  ensure  => file,
+  owner   => 'root',
+  group   => 'root',
+  mode    => '0644',
+  content => "server {
+    listen 80 default_server;
+    listen [::]:80 default_server;
+
+    root   /etc/nginx/html;
+    index  index.html index.htm;
+
+    location /redirect_me {
+        return 301 https://www.youtube.com/watch?v=QH2-TGUlwu4;
+    }
+
+    error_page 404 /404.html;
+    location = /404.html {
+        internal;
+    }
+  }",
+  notify  => Service['nginx'], # Notify the nginx service to restart if the configuration file changes
+}
+
+# Ensure the configuration is linked from sites-enabled
+file { '/etc/nginx/sites-enabled/default':
+  ensure => link,
+  target => '/etc/nginx/sites-available/default',
+  notify => Service['nginx'],
 }
