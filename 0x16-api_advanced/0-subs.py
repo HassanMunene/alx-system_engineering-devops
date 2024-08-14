cb2fb873085664ab8c0bd7c395a0cb974c99a72b
HassanMunene <awanzihassan@gmail.com>
2024-08-14 23:43:01 +0300

    corrections

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,39 +1,17 @@
 #!/usr/bin/python3
 """
-subs
+contains the number_of_subscribers function
 """
 
 import requests
 
 
 def number_of_subscribers(subreddit):
-    # Defines the URL for the subreddit's about.json endpoint
-    url = f'https://www.reddit.com/r/{subreddit}/about.json'
-
-    # Set's a custom User-Agent to avoid "Too Many Requests" errors
-    headers = {'User-Agent': 'My Reddit API Client'}
-
-    try:
-        # HTTP GET request
-        response = requests.get(url, headers=headers)
-        response.raise_for_status()  # Raise an exception for HTTP errors
-
-        data = response.json()
-
-        # Extract the number of subscribers from the JSON data
-        subscribers = data['data']['subscribers']
-
-        return subscribers
-    except requests.exceptions.RequestException:
-        # Handle request-related exceptions
+    """returns the number of subscriberes for a given subreddit"""
+    if subreddit is None or not isinstance(subreddit, str):
         return 0
-    except KeyError:
-        # Handle JSON parsing errors or missing data
-        return 0
-
-
-if __name__ == '__main__':
-
-    subreddit = "cybersecurity"  # Replace with the subreddit you want to check
-    subscribers = number_of_subscribers(subreddit)
-    print(subscribers)
+    r = requests.get('http://www.reddit.com/r/{}/about.json'.format(subreddit),
+                     headers={'User-Agent': 'Python/requests:APIproject:\
+                              v1.0.0 (by /u/fraol21)'}).json()
+    subs = r.get("data", {}).get("subscribers", 0)
+    return subs
